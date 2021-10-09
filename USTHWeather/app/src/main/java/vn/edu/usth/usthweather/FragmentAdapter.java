c9f3de9fe73231fe076abbdfb1c1c353235fa7be
hm1905 <nghoangminh1905@gmail.com>
2021-10-09 17:40:12 +0700

    Update for lab7

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/FragmentAdapter.java
@@ -0,0 +1,37 @@
+package vn.edu.usth.usthweather;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import vn.edu.usth.usthweather.WeatherAndForecast;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    private final int PAGE_COUNT = 3;
+    private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new WeatherAndForecast();
+            case 1: return new WeatherAndForecast();
+            case 2: return new WeatherAndForecast();
+        }
+        return new WeatherAndForecast();
+    }
+
+    @Override
+    public int getItemCount() {
+        return PAGE_COUNT;
+    }
+
+}
